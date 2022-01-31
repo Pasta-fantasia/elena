8abf71fe7cd2c1cbf029eec5a429c878a02915dc
Pere Jover <pjover@gmail.com>
2022-01-31 19:08:03 +0100

    Make other methods private

--- elena/elena_bot.py
+++ elena/elena_bot.py
@@ -12,24 +12,25 @@ class Elena:
         self._robot_filename = robot_filename
         self._exchange = exchange
 
-    def read_state(self):
+    def _read_state(self):
         fp = open(self._robot_filename, 'r')
         state = json.load(fp)
         fp.close()
         return state
 
-    def save_state(self, state, filename=None):
+    def _save_state(self, state, filename=None):
         if not filename:
             filename = self._robot_filename
         fp = open(filename, 'w')
         json.dump(state, fp)
         fp.close()
 
-    def estimate_buy_sel(self, state):
+    def _estimate_buy_sel(self, state):
         candles_df = self._exchange.get_candles(p_symbol=state['symbol'], p_limit=state['data_samples'])
-        return self.buy_sell(candles_df, state['algo'], state['margin'], state['tendence_tolerance'])
+        return self._buy_sell(candles_df, state['algo'], state['margin'], state['tendence_tolerance'])
 
-    def buy_sell(self, candles_df_buy_sell, algo, margin, tendence_tolerance):
+    @staticmethod
+    def _buy_sell(candles_df_buy_sell, algo, margin, tendence_tolerance):
         sell = 0
         buy = 0
         avg_price = candles_df_buy_sell["Close"].mean()
@@ -86,27 +87,27 @@ class Elena:
         return buy, sell
 
     @staticmethod
-    def sleep_until(sell_execution_time, minutes):
+    def _sleep_until(sell_execution_time, minutes):
         return sell_execution_time + minutes * 60 * 1000  # 45' after the sale
 
     def iterate(self):
-        state = self.read_state()
-        if state['sleep_until'] < get_time():
+        state = self._read_state()
+        if state['_sleep_until'] < get_time():
 
             if not state['buy_order_id'] and state['active']:
-                buy, sell = self.estimate_buy_sel(state)
+                buy, sell = self._estimate_buy_sel(state)
                 if buy > 0:
                     llog("create a new buy order")
                     new_buy_order_id = self._exchange.create_buy_order(state['max_order'], state['symbol'], buy)
-                    state['sleep_until'] = 0
+                    state['_sleep_until'] = 0
                     state['buy_order_id'] = new_buy_order_id
                     state['sell_order_id'] = ''
                     state['buy'] = buy
                     state['sell'] = sell
-                    self.save_state(state)
+                    self._save_state(state)
                 else:
-                    state['sleep_until'] = self.sleep_until(get_time(), 5)
-                    self.save_state(state)
+                    state['_sleep_until'] = self._sleep_until(get_time(), 5)
+                    self._save_state(state)
                     llog("don't buy")
                 return
 
@@ -116,50 +117,50 @@ class Elena:
                 if new_sell_order_id:
                     llog("create a new sell order")
                     state['sell_order_id'] = new_sell_order_id
-                    self.save_state(state)
+                    self._save_state(state)
                 else:
                     status, order_update_time = self._exchange.check_order_status(state['symbol'],
                                                                                   state['buy_order_id'])
                     if not status == OrderStatus.CANCELED.value:
-                        state['sleep_until'] = self.sleep_until(get_time(), 5)
-                        self.save_state(state)
+                        state['_sleep_until'] = self._sleep_until(get_time(), 5)
+                        self._save_state(state)
                         llog("waiting purchase")
                     else:
                         llog("buy cancellation")
                         filename = f"history/{str(get_time())}_{str(state['buy_order_id'])}.json"
-                        self.save_state(state, filename)
-                        state['sleep_until'] = 0
+                        self._save_state(state, filename)
+                        state['_sleep_until'] = 0
                         state['buy_order_id'] = ''
                         state['sell_order_id'] = ''
                         state['buy'] = 0
                         state['sell'] = 0
-                        self.save_state(state)
+                        self._save_state(state)
                 return
 
             if state['buy_order_id'] and state['sell_order_id']:
                 status, order_update_time = self._exchange.check_order_status(state['symbol'], state['sell_order_id'])
                 if status == OrderStatus.FILLED.value:
                     llog("save history")
-                    self.save_state('history/' + str(get_time()) + '_' + str(state['buy_order_id']) + '.json', state)
+                    self._save_state('history/' + str(get_time()) + '_' + str(state['buy_order_id']) + '.json', state)
                     llog("set sleep")
-                    state['sleep_until'] = self.sleep_until(order_update_time, state['data_samples'] * 1.5)
+                    state['_sleep_until'] = self._sleep_until(order_update_time, state['data_samples'] * 1.5)
                     state['buy_order_id'] = ''
                     state['sell_order_id'] = ''
                     state['buy'] = 0
                     state['sell'] = 0
-                    self.save_state(state)
+                    self._save_state(state)
                 elif status == OrderStatus.CANCELED.value:
                     llog("sell cancellation, save history")
-                    self.save_state('history/' + str(get_time()) + '_' + str(state['buy_order_id']) + '.json', state)
+                    self._save_state('history/' + str(get_time()) + '_' + str(state['buy_order_id']) + '.json', state)
                     state['active'] = 0
                     state['buy_order_id'] = ''
                     state['sell_order_id'] = ''
                     state['buy'] = 0
                     state['sell'] = 0
-                    self.save_state(state)
+                    self._save_state(state)
                 else:
-                    state['sleep_until'] = self.sleep_until(get_time(), 15)
-                    self.save_state(state)
+                    state['_sleep_until'] = self._sleep_until(get_time(), 15)
+                    self._save_state(state)
                     llog("waiting sell")
                 return
         else:
