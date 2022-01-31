e6ba887677778eb3539ea2bdfc7cb12a42e44dd4
Fran Simó <fransimo@gmail.com>
2022-01-31 19:50:28 +0100

    refactor _ensure_sell_is_higher_than_buy_by

--- elena/elena_bot.py
+++ elena/elena_bot.py
@@ -30,6 +30,14 @@ class Elena:
         return self._buy_sell(candles_df, state['algo'], state['margin'], state['tendence_tolerance'])
 
     @staticmethod
+    def _ensure_sell_is_higher_than_buy_by(next_sell, next_buy, buy, sell, minimum_profit=1.0005):
+        # ensure sell is higher than buy at least by 5 per thousand to pay fees
+        if next_sell / next_buy > minimum_profit:
+            buy = next_buy
+            sell = next_sell
+        return buy, sell
+
+    @staticmethod
     def _buy_sell(candles_df_buy_sell, algo, margin, tendence_tolerance):
         sell = 0
         buy = 0
@@ -41,7 +49,6 @@ class Elena:
 
         if tendency > tendence_tolerance:
             if algo == 0:
-                # TODO Pair review: extract methods?
                 margin_local = margin * (2 / 3)
                 buy = avg_price * (1 - ((margin_local / 2) / 100))
                 buy = buy.astype(float)
@@ -55,40 +62,32 @@ class Elena:
                 buy = buy.astype(float)
                 sell = buy * (1 + (margin / 100))
             if algo == 4:
-                # TODO Pair review: duplicated code with lines 80-90
+                # TODO: delete 3 or 4 depending on simultaion results
                 regression_low = np.polyfit(candles_df_buy_sell.index, candles_df_buy_sell["Low"], 1)
                 next_low = regression_low[0] * (candles_df_buy_sell.index[-1] + 1) + regression_low[1]
 
                 regression_close = np.polyfit(candles_df_buy_sell.index, candles_df_buy_sell["Close"], 1)
                 next_close = regression_close[0] * (candles_df_buy_sell.index[-1] + 1) + regression_close[1]
 
-                # TODO Pair review: review logic and extract method
-                if next_close / next_low > 1.0005:  # ensure sell is higher than buy at least by 5 per thousand to pay fees
-                    buy = next_low
-                    sell = next_close
+                buy, sell = Elena._ensure_sell_is_higher_than_buy_by(next_close, next_low, buy, sell)
+
             if algo == 5:
                 regression_low = np.polyfit(candles_df_buy_sell.index, candles_df_buy_sell["Low"], 1)
                 next_low = regression_low[0] * (candles_df_buy_sell.index[-1] + 1) + regression_low[1]
 
-                regression_close = np.polyfit(candles_df_buy_sell.index, candles_df_buy_sell["High"], 1)
-                next_high = regression_close[0] * (candles_df_buy_sell.index[-1] + 1) + regression_close[1]
+                regression_high = np.polyfit(candles_df_buy_sell.index, candles_df_buy_sell["High"], 1)
+                next_high = regression_high[0] * (candles_df_buy_sell.index[-1] + 1) + regression_high[1]
 
-                # TODO Pair review: review logic and extract method
-                if next_high / next_low > 1.0005:  # ensure sell is higher than buy at least by 5 per thousand to pay fees
-                    buy = next_low
-                    sell = next_high
+                buy, sell = Elena._ensure_sell_is_higher_than_buy_by(next_high, next_low, buy, sell)
+
         if algo == 3:
-            # TODO Pair review: duplicated code with lines 57-67
             regression_low = np.polyfit(candles_df_buy_sell.index, candles_df_buy_sell["Low"], 1)
             next_low = regression_low[0] * (candles_df_buy_sell.index[-1] + 1) + regression_low[1]
 
             regression_close = np.polyfit(candles_df_buy_sell.index, candles_df_buy_sell["Close"], 1)
             next_close = regression_close[0] * (candles_df_buy_sell.index[-1] + 1) + regression_close[1]
 
-            # TODO Pair review: review logic and extract method
-            if next_close / next_low > 1.0005:  # ensure sell is higher than buy at least by 5 per thousand to pay fees
-                buy = next_low
-                sell = next_close
+            buy, sell = Elena._ensure_sell_is_higher_than_buy_by(next_close, next_low, buy, sell)
 
         return buy, sell
 
