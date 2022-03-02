69df7ee74ecca22708d59b2d80327dedfdf6f26b
Fran Simó <fransimo@gmail.com>
2022-03-02 18:05:57 +0100

    testing

--- elena/elena_bot.py
+++ elena/elena_bot.py
@@ -147,8 +147,7 @@ class Elena:
         if state.get('buy_auto_cancel_timeout') is None:
             state['buy_auto_cancel_timeout'] = 120
         if state.get('sell_auto_cancel_timeout') is None:
-            state['sell_auto_cancel_timeout'] = 0  # 1 for testing
-        state['sell_auto_cancel_timeout'] = state['data_samples']   # 1 for testing
+            state['sell_auto_cancel_timeout'] = 0   # state['data_samples']  for testing
         if state.get('stop_loss_percentage') is None:
             state['stop_loss_percentage'] = 0
         if state.get('reinvest') is None:
