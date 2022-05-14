9e7e073937dc50818bb0c4bb54f5b9a2fc5f1a60
Fran Simó <fransimo@gmail.com>
2022-05-14 13:55:03 +0200

    .bashrc

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ python3 -m venv $HOME/venv
 
 source $HOME/venv/bin/activate
 
+echo "source $HOME/venv/bin/activate " >> $HOME/.bashrc
+
 pip install -U pip setuptools wheel
 
 pip install -r requirements.txt
