3875c7e90dd672ec203131a86246db62869ca622
Pere Jover <pjover@gmail.com>
2023-07-29 08:34:31 +0200

    Log Elena version, instead of print

--- elena/entrypoint/local/entrypoint.py
+++ elena/entrypoint/local/entrypoint.py
@@ -1,6 +1,6 @@
+from elena import __version__ as version
 from elena.adapters.config.local_config_reader import LocalConfigReader
 from elena.config import dependency_injection
-from elena import __version__ as version
 
 
 def main():
@@ -8,7 +8,7 @@ def main():
     container = dependency_injection.get_container(config)
     container.wire(modules=[__name__])
     elena = container.elena()
-    print(f"Starting Elena v{version} from CLI")
+    container.logger().info("Starting Elena v%s from CLI", version)
     elena.run()
 
 
