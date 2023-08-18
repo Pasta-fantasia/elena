9156119ff763edbeee886f5681a5808edc7cc188
Fran Simó <fransimo@gmail.com>
2023-08-18 11:07:05 +0200

    class Trade

--- elena/domain/model/trade.py
+++ elena/domain/model/trade.py
@@ -5,24 +5,24 @@ from pydantic.config import Enum
 
 from elena.domain.model.exchange import ExchangeType
 from elena.domain.model.trading_pair import TradingPair
-from elena.domain.model.order import Order
+import time
 
 
 class Trade(BaseModel):
-    id: str
+    id: str = str(int(time.time() * 1000))  # all id are str
     exchange_id: ExchangeType
     bot_id: str
     strategy_id: str
     pair: TradingPair
     size: float  # ordered amount of base currency
 
-    entry_time: int  # order placing/opening Unix timestamp in milliseconds
+    entry_time: int = int(time.time() * 1000)  # order placing/opening Unix timestamp in milliseconds
     entry_price: float
-    entry_order: Order
+    entry_order_id: Optional[str] = 'manual'
 
-    exit_time: int  # order placing/opening Unix timestamp in milliseconds
-    exit_price: float
-    exit_order: Order
+    exit_time: Optional[int] = 0  # order placing/opening Unix timestamp in milliseconds
+    exit_price: Optional[float] = 0
+    exit_order_id: Optional[str] = 'manual'
 
-    duration: int
-    return_pct: float
+    duration: Optional[int] = 0
+    return_pct: Optional[float] = 0
