593b8023aff89a6858ea99d4a714cd543d27a983
Pere Jover <pjover@gmail.com>
2023-12-02 08:17:14 +0100

    Create test and start recording exchange data for tests

---
+++ test/elena/domain/services/fake_exchange_manager.py
@@ -0,0 +1,98 @@
+import json
+from os.path import dirname, join, realpath
+from typing import Dict, Optional
+
+import pandas as pd
+
+from elena.adapters.exchange_manager.cctx_exchange_manager import \
+    CctxExchangeManager
+from elena.domain.model.balance import Balance
+from elena.domain.model.bot_config import BotConfig
+from elena.domain.model.exchange import Exchange
+from elena.domain.model.order import Order, OrderSide, OrderType
+from elena.domain.model.order_book import OrderBook
+from elena.domain.model.time_frame import TimeFrame
+from elena.domain.model.trading_pair import TradingPair
+from elena.domain.ports.exchange_manager import ExchangeManager
+from elena.domain.ports.logger import Logger
+
+
+class FakeExchangeManager(ExchangeManager):
+    def __init__(self, config: Dict, logger: Logger):
+        self._cctx = CctxExchangeManager(config, logger)
+        self._logger = logger
+
+    @staticmethod
+    def _load_from_json(filename: str) -> Dict:
+        _filename = join(dirname(realpath(__file__)), "data", f"{filename}.json")
+        with open(_filename, "r") as f:
+            return json.load(f)
+
+    @staticmethod
+    def save_to_json(cache: Dict, filename: str):
+        _filename = join(dirname(realpath(__file__)), "data", f"{filename}.json")
+        with open(_filename, "w") as f:
+            json.dump(cache, f)
+
+    def read_candles(
+        self,
+        exchange: Exchange,
+        pair: TradingPair,
+        time_frame: TimeFrame = TimeFrame.min_1,  # type: ignore
+    ) -> pd.DataFrame:
+        data = self._load_from_json("read_candles")
+        result = pd.DataFrame.from_dict(data)
+        return result
+
+    def amount_to_precision(
+        self, exchange: Exchange, pair: TradingPair, amount: float
+    ) -> float:
+        result = self._cctx.amount_to_precision(exchange, pair, amount)
+        return result
+
+    def price_to_precision(
+        self, exchange: Exchange, pair: TradingPair, price: float
+    ) -> float:
+        result = self._cctx.price_to_precision(exchange, pair, price)
+        return result
+
+    def read_order_book(self, exchange: Exchange, pair: TradingPair) -> OrderBook:
+        result = self._cctx.read_order_book(exchange, pair)
+        self.save_to_json(result.dict(), "read_order_book")
+        return result
+
+    def get_balance(self, exchange: Exchange) -> Balance:
+        data = self._load_from_json("get_balance")
+        result = Balance.parse_obj(data)
+        return result
+
+    def place_order(
+        self,
+        exchange: Exchange,
+        bot_config: BotConfig,
+        order_type: OrderType,
+        side: OrderSide,
+        amount: float,
+        price: Optional[float] = None,
+        params: Optional[Dict] = {},
+    ) -> Order:
+        result = self._cctx.place_order(
+            exchange, bot_config, order_type, side, amount, price, params
+        )
+        self.save_to_json(result.dict(), "place_order")
+        return result
+
+    def cancel_order(self, exchange: Exchange, bot_config: BotConfig, order_id: str):
+        result = self._cctx.cancel_order(exchange, bot_config, order_id)
+        self.save_to_json(result.dict(), "cancel_order")
+        return result
+
+    def fetch_order(
+        self, exchange: Exchange, bot_config: BotConfig, order_id: str
+    ) -> Order:
+        result = self._cctx.fetch_order(exchange, bot_config, order_id)
+        self.save_to_json(result.dict(), "fetch_order")
+        return result
+
+    def limit_min_amount(self, exchange: Exchange, pair: TradingPair) -> float:
+        return 1e-05
--- test/elena/domain/services/test_elena.py
+++ test/elena/domain/services/test_elena.py
@@ -1,14 +1,21 @@
+import pathlib
 from logging import Logger
+from test.elena.domain.services.fake_exchange_manager import \
+    FakeExchangeManager
 
+from elena.adapters.bot_manager.local_bot_manager import LocalBotManager
+from elena.adapters.config.local_config_reader import LocalConfigReader
+from elena.adapters.logger.local_logger import LocalLogger
 from elena.domain.model.bot_config import BotConfig
 from elena.domain.model.bot_status import BotStatus
-from elena.domain.model.time_frame import TimeFrame
 from elena.domain.model.exchange import Exchange
+from elena.domain.model.time_frame import TimeFrame
 from elena.domain.ports.bot import Bot
 from elena.domain.ports.strategy_manager import StrategyManager
+from elena.domain.services.elena import Elena
 
 
-class ExchangeBasicOperationsTest(Bot):
+class ExchangeBasicOperationsBot(Bot):
     _manager: StrategyManager
     _logger: Logger
     _config: BotConfig
@@ -22,13 +29,17 @@ class ExchangeBasicOperationsTest(Bot):
         self._logger = logger
         self._name = self.__class__.__name__
         self._config = bot_config
-        self._exchange = self._manager.get_exchange(self._config.exchange_id)
+
+        exchange = self._manager.get_exchange(self._config.exchange_id)
+        if not exchange:
+            raise Exception(f"Cannot get Exchange from {self._config.exchange_id} ID")
+        self._exchange = exchange
 
         try:
-            self.band_length = bot_config.config['band_length']
-            self.band_mult = bot_config.config['band_mult']
+            self.band_length = bot_config.config["band_length"]
+            self.band_mult = bot_config.config["band_mult"]
         except Exception as err:
-            logger.error('Error initializing Bot config')
+            logger.error("Error initializing Bot config")
             logger.error(f"Unexpected {err=}, {type(err)=}")
 
     def next(self, status: BotStatus) -> BotStatus:
@@ -36,7 +47,9 @@ class ExchangeBasicOperationsTest(Bot):
 
         # Verify that the exchange is in sandbox mode!!!!
         if not self._exchange.sandbox_mode:
-            raise Exception("Exchange is not in sandbox mode, this strategy is ment for testing only!")
+            raise Exception(
+                "Exchange is not in sandbox mode, this strategy is ment for testing only!"
+            )
 
         # is there any free balance to handle?
         balance = self._manager.get_balance(self._exchange)
@@ -53,7 +66,9 @@ class ExchangeBasicOperationsTest(Bot):
         min_amount = self._manager.limit_min_amount(self._exchange, self._config.pair)
 
         # get candles
-        candles = self._manager.read_candles(self._exchange, self._config.pair, TimeFrame.day_1)
+        candles = self._manager.read_candles(
+            self._exchange, self._config.pair, TimeFrame.day_1  # type: ignore
+        )
 
         # correct precisions for exchange
         # new_stop_loss = self._manager.price_to_precision(self._exchange, self._bot_config.pair, new_stop_loss)
@@ -65,3 +80,20 @@ class ExchangeBasicOperationsTest(Bot):
         # can we know the open orders? if can => cancel any and create some stop_loss
 
         return status
+
+
+def test_elena():
+    home = pathlib.Path(__file__).parent.parent.parent.__str__()
+    config = LocalConfigReader(home).config
+    logger = LocalLogger(config)
+    bot_manager = LocalBotManager(config, logger)
+    exchange_manager = FakeExchangeManager(config, logger)
+
+    sut = Elena(
+        config=config,
+        logger=logger,
+        bot_manager=bot_manager,
+        exchange_manager=exchange_manager,
+    )
+
+    sut.run()
