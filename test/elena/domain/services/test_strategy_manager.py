d2aca185c250b6a0ab46ce451bffc6e11642829a
Pere Jover <pjover@gmail.com>
2023-12-03 07:35:13 +0100

    Improve run when cron expression test

--- test/elena/domain/services/test_strategy_manager.py
+++ test/elena/domain/services/test_strategy_manager.py
@@ -1,70 +1,50 @@
 from datetime import datetime
 from unittest.mock import patch
 
+import pytest
+
 from elena.domain.services.strategy_manager import StrategyManagerImpl
 
 
-def test__check_if_bot_has_to_run():
-    """
-    With a cron expression configured to run every 5th minute
-    the last execution time set to 10:05:00
-    and patching the current time to 10:10:00
-    the bot should run.
-    """
-    last_execution = datetime(2020, 1, 1, 10, 5, 0)
+@pytest.mark.parametrize(
+    "name,last_execution, now, expected",
+    [
+        (
+            "Bot has to run, o'clock",
+            datetime(2020, 1, 1, 10, 5, 0),
+            datetime(2020, 1, 1, 10, 10, 0),
+            True,
+        ),
+        (
+            "Bot has to run, 1 second after",
+            datetime(2020, 1, 1, 10, 5, 0),
+            datetime(2020, 1, 1, 10, 10, 1),
+            True,
+        ),
+        (
+            "Bot has to run, one intermediate run is missing",
+            datetime(2020, 1, 1, 10, 5, 0),
+            datetime(2020, 1, 1, 20, 11, 0),
+            True,
+        ),
+        (
+            "Bot has to do not run",
+            datetime(2020, 1, 1, 10, 0, 0),
+            datetime(2020, 1, 1, 10, 1, 0),
+            False,
+        ),
+        (
+            "Bot has to do not run by one second",
+            datetime(2020, 1, 1, 10, 5, 0),
+            datetime(2020, 1, 1, 10, 9, 59),
+            False,
+        ),
+    ],
+)
+def test_check_if_bot_has_to_run(name, last_execution, now, expected):
     with (patch("elena.domain.services.strategy_manager.datetime") as mock_datetime):
-        mock_datetime.now.return_value = datetime(2020, 1, 1, 10, 10, 0)
+        mock_datetime.now.return_value = now
         actual = StrategyManagerImpl._check_if_bot_has_to_run(
             last_execution=last_execution, cron_expression="*/5 * * * *"
         )
-    assert actual is True
-
-
-def test__check_if_bot_has_to_run_one_intermediate_run_is_missing():
-    """
-    With a cron expression configured to run every 5th minute
-    the last execution time set to 10:05:00
-    and patching the current time to 10:20:00
-    the bot should run.
-    """
-    last_execution = datetime(2020, 1, 1, 10, 5, 0)
-    with (patch("elena.domain.services.strategy_manager.datetime") as mock_datetime):
-        mock_datetime.now.return_value = datetime(2020, 1, 1, 20, 10, 0)
-        actual = StrategyManagerImpl._check_if_bot_has_to_run(
-            last_execution=last_execution, cron_expression="*/5 * * * *"
-        )
-    assert actual is True
-
-
-def test__check_if_bot_has_to_do_not_run():
-    """
-    With a cron expression configured to run every 5th minute
-    the last execution time set to 10:05:00
-    and patching the current time to 10:09:00
-    the bot should run.
-    """
-    last_execution = datetime(2020, 1, 1, 10, 5, 0)
-    with patch("elena.domain.services.strategy_manager.datetime") as mock_datetime:
-        mock_datetime.now.return_value = datetime(2020, 1, 1, 10, 9, 0)
-        actual = StrategyManagerImpl._check_if_bot_has_to_run(
-            last_execution=last_execution, cron_expression="*/5 * * * *"
-        )
-
-    assert actual is False
-
-
-def test__check_if_bot_has_to_do_not_run_by_one_second():
-    """
-    With a cron expression configured to run every 5th minute
-    the last execution time set to 10:05:00
-    and patching the current time to 10:09:59
-    the bot should run.
-    """
-    last_execution = datetime(2020, 1, 1, 10, 5, 0)
-    with patch("elena.domain.services.strategy_manager.datetime") as mock_datetime:
-        mock_datetime.now.return_value = datetime(2020, 1, 1, 10, 9, 59)
-        actual = StrategyManagerImpl._check_if_bot_has_to_run(
-            last_execution=last_execution, cron_expression="*/5 * * * *"
-        )
-
-    assert actual is False
+    assert actual is expected, f"Error running test '{name}'"
