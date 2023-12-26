be92ef78fb9a3f257fb51935d4e427e9fff5785f
Fran Simó <fransimo@gmail.com>
2023-12-26 18:51:59 +0100

    defaults for get_elena_instance

--- elena/entrypoint/local/entrypoint.py
+++ elena/entrypoint/local/entrypoint.py
@@ -3,8 +3,5 @@ import os
 from elena.domain.services.elena import get_elena_instance
 
 if __name__ == "__main__":
-    elena = get_elena_instance(
-        config_manager_class_path="elena.adapters.config.local_config_manager.LocalConfigManager",
-        config_manager_url=os.environ.get("ELENA_HOME", os.getcwd()),
-    )
+    elena = get_elena_instance()
     elena.run()
