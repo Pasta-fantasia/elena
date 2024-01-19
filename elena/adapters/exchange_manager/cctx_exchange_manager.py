d4fb82e2c876807649bd8b13a91025b84a6f693d
Pere Jover <pjover@gmail.com>
2024-01-19 20:22:42 +0100

    Do not store dataframes
    #57

--- elena/adapters/exchange_manager/cctx_exchange_manager.py
+++ elena/adapters/exchange_manager/cctx_exchange_manager.py
@@ -176,26 +176,9 @@ class CctxExchangeManager(ExchangeManager):
             exchange.id,
             pair,
         )
-        candles_dataframe_id = self._get_dataframe_id(exchange, pair, time_frame)
-
-        try:
-            stored_candles = self._storage_manager.load_data_frame(candles_dataframe_id)
-        except Exception:
-            stored_candles = pd.DataFrame()
-
-        if not stored_candles.empty and self._are_stored_candles_up_to_date(stored_candles, time_frame, datetime.now()):
-            # If the stored candles are up-to-date, return them
-            return stored_candles
-
         conn = self._connect(exchange)
         candles = self._fetch_candles(conn, pair, time_frame)
         self._logger.info("Read %d %s candles from %s", candles.shape[0], pair, exchange.id.value)
-
-        try:
-            self._storage_manager.save_data_frame(candles_dataframe_id, candles)
-        except Exception as err:
-            self._logger.error("Error saving candles: %s", err, exc_info=1)
-
         return candles
 
     @staticmethod
